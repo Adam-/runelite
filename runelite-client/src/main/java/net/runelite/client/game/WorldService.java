c68b00c35d483fa4e6639488e88aeb2f68ee43c3
Adam <Adam@sigterm.info>
2019-11-24 14:03:40 -0500

    work on world service

---
+++ runelite-client/src/main/java/net/runelite/client/game/WorldService.java
@@ -0,0 +1,58 @@
+package net.runelite.client.game;
+
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import javax.annotation.Nullable;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.http.api.worlds.World;
+import net.runelite.http.api.worlds.WorldClient;
+import net.runelite.http.api.worlds.WorldResult;
+
+@Singleton
+@Slf4j
+public class WorldService
+{
+	private static final int WORLD_FETCH_TIMER = 10; // minutes
+
+	private final ScheduledExecutorService scheduledExecutorService;
+	private final WorldClient worldClient;
+
+	@Getter
+	@Nullable
+	private WorldResult worlds;
+
+	//@Getter
+	//private Instant lastLookup = Instant.now();
+
+	@Inject
+	private WorldService(ScheduledExecutorService scheduledExecutorService, WorldClient worldClient) {
+		this.scheduledExecutorService = scheduledExecutorService;
+		this.worldClient = worldClient;
+
+		scheduledExecutorService.scheduleWithFixedDelay(this::tick, 0, 10, TimeUnit.MINUTES);
+	}
+
+	private void tick() {
+		log.debug("Fetching worlds");
+
+		try
+		{
+			WorldResult worldResult = worldClient.lookupWorlds();
+			worldResult.getWorlds().sort(Comparator.comparingInt(World::getId));
+			worlds = worldResult;
+		}
+		catch (IOException ex)
+		{
+			log.warn("Error looking up worlds", ex);
+		}
+	}
+
+	public void refresh() {
+		scheduledExecutorService.execute(this::tick);
+	}
+}
