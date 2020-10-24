f2c291b49c4e2c11fd30ad8eb8026aef88d66e3e
Tomas Slusny <slusnucky@gmail.com>
2020-10-24 17:59:58 +0200

    Fix build script settings.xml reference
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- ci/build.sh
+++ ci/build.sh
@@ -6,4 +6,4 @@ wget -O/tmp/glslang.zip 'https://github.com/KhronosGroup/glslang/releases/downlo
 echo '9adcfdef5b52275e61068aafbb62747936c6c18ab6dc32a6ef707cfc7b0df423  /tmp/glslang.zip' | sha256sum -c
 unzip -q /tmp/glslang.zip -d /tmp/glslang
 
-mvn clean install --settings travis/settings.xml -Dglslang.path=/tmp/glslang/bin/glslangValidator
+mvn clean install --settings ci/settings.xml -Dglslang.path=/tmp/glslang/bin/glslangValidator
