6feae5b405a74e23f55e26e29354e4d2ea412fac
Adam <Adam@sigterm.info>
2022-06-18 21:30:56 -0400

    pmd exclude party

--- runelite-client/src/main/java/net/runelite/client/party/WSClient.java
+++ runelite-client/src/main/java/net/runelite/client/party/WSClient.java
@@ -163,8 +163,8 @@ public class WSClient extends WebSocketListener implements AutoCloseable
 
 	void send(WebsocketMessage message)
 	{
+		log.debug("Sending: {}", message);
 		final String json = gson.toJson(message, WebsocketMessage.class);
-		log.debug("Sending: {}", json);
 		final Party.Data data = Party.Data.newBuilder()
 			.setData(com.google.protobuf.ByteString.copyFromUtf8(json))
 			.build();
