d98e8a39993e70c81432e33c9974f84a15e1bd12
Adam <Adam@sigterm.info>
2023-02-11 13:06:08 -0500

    headers

--- runelite-client/src/main/java/net/runelite/client/config/ConfigManager.java
+++ runelite-client/src/main/java/net/runelite/client/config/ConfigManager.java
@@ -148,7 +148,7 @@ public class ConfigManager
 		this.profileManager = profileManager;
 		this.sessionManager = sessionManager;
 
-		scheduledExecutorService.scheduleWithFixedDelay(this::sendConfig, 30, 15, TimeUnit.SECONDS);
+		scheduledExecutorService.scheduleWithFixedDelay(this::sendConfig, 30, 15, TimeUnit.SECONDS);///XXX CHANGE THIS BACK
 	}
 
 	public void switchProfile(ConfigProfile newProfile)
@@ -1418,6 +1418,4 @@ public class ConfigManager
 			profile = key.substring(0, i);
 			key = key.substring(i + 1);
 		}
-		return new String[]{group, profile, key};
-	}
-}
+		return new String[]{grou
