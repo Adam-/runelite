7965c66a94815171927886e6ec5dae1c0b6478bd
Adam <Adam@sigterm.info>
2024-05-10 13:06:58 -0400

    combat level: set precise combat level text each tick
    This avoids it breifly showing the non precise combat level if viewed before the next server tick

--- runelite-client/src/main/java/net/runelite/client/plugins/combatlevel/CombatLevelPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/combatlevel/CombatLevelPlugin.java
@@ -33,11 +33,10 @@ import java.util.regex.Pattern;
 import javax.inject.Inject;
 import net.runelite.api.Client;
 import net.runelite.api.Experience;
-import net.runelite.api.GameState;
 import net.runelite.api.ScriptID;
 import net.runelite.api.Skill;
 import net.runelite.api.WorldType;
-import net.runelite.api.events.GameTick;
+import net.runelite.api.events.ClientTick;
 import net.runelite.api.events.ScriptPostFired;
 import net.runelite.api.events.ScriptPreFired;
 import net.runelite.api.events.StatChanged;
@@ -79,6 +78,8 @@ public class CombatLevelPlugin extends Plugin
 	@Inject
 	private OverlayManager overlayManager;
 
+	private String combatLevelStr;
+
 	@Provides
 	CombatLevelConfig provideConfig(ConfigManager configManager)
 	{
@@ -88,6 +89,17 @@ public class CombatLevelPlugin extends Plugin
 	@Override
 	protected void startUp() throws Exception
 	{
+		double combatLevel = Experience.getCombatLevelPrecise(
+			client.getRealSkillLevel(Skill.ATTACK),
+			client.getRealSkillLevel(Skill.STRENGTH),
+			client.getRealSkillLevel(Skill.DEFENCE),
+			client.getRealSkillLevel(Skill.HITPOINTS),
+			client.getRealSkillLevel(Skill.MAGIC),
+			client.getRealSkillLevel(Skill.RANGED),
+			client.getRealSkillLevel(Skill.PRAYER)
+		);
+		combatLevelStr = DECIMAL_FORMAT.format(combatLevel);
+
 		overlayManager.add(overlay);
 
 		if (config.wildernessAttackLevelRange())
@@ -122,7 +134,7 @@ public class CombatLevelPlugin extends Plugin
 		if (skill == Skill.ATTACK || skill == Skill.DEFENCE || skill == Skill.STRENGTH || skill == Skill.HITPOINTS
 			|| skill == Skill.MAGIC || skill == Skill.PRAYER || skill == Skill.RANGED)
 		{
-			double combatLevelPrecise = Experience.getCombatLevelPrecise(
+			double combatLevel = Experience.getCombatLevelPrecise(
 				client.getRealSkillLevel(Skill.ATTACK),
 				client.getRealSkillLevel(Skill.STRENGTH),
 				client.getRealSkillLevel(Skill.DEFENCE),
@@ -131,34 +143,20 @@ public class CombatLevelPlugin extends Plugin
 				client.getRealSkillLevel(Skill.RANGED),
 				client.getRealSkillLevel(Skill.PRAYER)
 			);
+			combatLevelStr = DECIMAL_FORMAT.format(combatLevel);
 		}
 	}
 
 	@Subscribe
-	public void onGameTick(GameTick event)
+	private void onClientTick(ClientTick tick)
 	{
-		if (client.getGameState() != GameState.LOGGED_IN)
-		{
-			return;
-		}
-
 		Widget combatLevelWidget = client.getWidget(ComponentID.COMBAT_LEVEL);
 		if (combatLevelWidget == null || !config.showPreciseCombatLevel())
 		{
 			return;
 		}
 
-		double combatLevelPrecise = Experience.getCombatLevelPrecise(
-				client.getRealSkillLevel(Skill.ATTACK),
-				client.getRealSkillLevel(Skill.STRENGTH),
-				client.getRealSkillLevel(Skill.DEFENCE),
-				client.getRealSkillLevel(Skill.HITPOINTS),
-				client.getRealSkillLevel(Skill.MAGIC),
-				client.getRealSkillLevel(Skill.RANGED),
-				client.getRealSkillLevel(Skill.PRAYER)
-		);
-
-		combatLevelWidget.setText("Combat Lvl: " + DECIMAL_FORMAT.format(combatLevelPrecise));
+		combatLevelWidget.setText("Combat Lvl: " + combatLevelStr);
 	}
 
 	@Subscribe
