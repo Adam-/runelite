370059f7a204ad2a027f21898a0b492f6c585d81
Adam <Adam@sigterm.info>
2026-07-26 12:18:43 -0400

    add a scope for each extension

--- runelite-client/src/main/java/net/runelite/client/plugins/gpu/Template.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/gpu/Template.java
@@ -81,7 +81,9 @@ class Template
 					}
 
 					sb.append("// START RUNELITE EXTENSION: ").append(extension.owner).append('\n');
+					sb.append('{').append('\n');
 					sb.append(code).append('\n');
+					sb.append('}').append('\n');
 					sb.append("// END   RUNELITE EXTENSION: ").append(extension.owner).append('\n');
 				}
 
